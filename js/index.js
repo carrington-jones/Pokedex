5edc7e8cf51e6a4126c2714087f3dfac9510fa14
Carrington Jones <carrington.jones47@gmail.com>
2021-09-29 17:46:05 -0500

    Able to render all pokemon to index.html page from API

--- js/index.js
+++ js/index.js
@@ -5,20 +5,21 @@ $(document).ready(function () {
     $('.pokemonSearchCard').hide()
 });
 
+
 const fetchPokemon = () => {
     const promises = []; // Start off with an empty array of promises
-    for(let i = 1; i <= 150; i++ ) {
+    for (let i = 1; i <= 150; i++) {
         const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(pokemonUrl).then((res) => res.json())); //For each one of our requests we push them to promise array
     }
     Promise.all(promises).then((results => {
-        const pokemon = results.map((data) =>({ //iterating through each result, going to get a reference to each one of those. With each one of those it then converts it to our built object
+        const pokemon = results.map((data) => ({ //iterating through each result, going to get a reference to each one of those. With each one of those it then converts it to our built object
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
             type: data.types.map(type => type.type.name).join(', ') //This grabs each name in type and creates a new array. It then joins them into a string.
         }));
-        console.log(pokemon)
+        displayPokemon(pokemon)
     }))
 };
 
@@ -43,3 +44,16 @@ function getPokemonData() {
             console.log(userPokemon)
         })
 }
+
+const displayPokemon = (pokemon => {
+    console.log(pokemon)
+    const pokemonHTMLString = pokemon.map(pokeman =>
+        `
+    <li>
+        <img src="${pokeman.image}"/>
+        <h2>${pokeman.id}.${pokeman.name}</h2>
+            <p>Type: ${pokeman.type}</p>
+        </li>
+        `)
+    $('#pokedex').html(pokemonHTMLString)
+    })
